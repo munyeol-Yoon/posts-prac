a099e6301c44f0b05b073ef962c2a39ffb2ab3ec
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-01 01:05:26 +0900

    create : 로그인 api 구현

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const { Users, UserInfos } = require("../models");
 
@@ -28,6 +29,37 @@ router.post("/", async (req, res) => {
     });
 
     return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// 로그인
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await Users.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "해당 사용자가 존재하지 않습니다." });
+    } else if (user.password !== password) {
+      return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+    }
+    const token = jwt.sign(
+      {
+        userId: user.userId,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("authorization", `Bearer ${token}`);
+
+    return res.status(200).json({ message: "로그인에 성공하였습니다." });
   } catch (err) {
     console.error(err);
   }
