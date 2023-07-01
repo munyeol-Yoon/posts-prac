7e3fa66005dd289149ff00904b1055c9c06f610f
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-07-01 17:03:56 +0900

    create : 게시글 목록 조회, 게시글 상세 조회 api 구현

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -23,4 +23,34 @@ router.post("/", authMiddleware, async (req, res) => {
   }
 });
 
+// 게시글 목록 조회
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const posts = await Posts.findAll({
+      attributes: ["postId", "title", "createdAt", "updatedAt"],
+      order: [["createdAt", "DESC"]],
+    });
+
+    return res.status(200).json({ data: posts });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// 게시글 상세 조회
+router.get("/:postId", authMiddleware, async (req, res) => {
+  try {
+    const { postId } = req.params;
+
+    const post = await Posts.findOne({
+      attributes: ["postId", "title", "content", "createdAt", "updatedAt"],
+      where: { postId },
+    });
+
+    return res.status(200).json({ data: post });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
 module.exports = router;
